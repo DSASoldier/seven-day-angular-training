120e407a1a4b3e45b6ae53ea386a786221a931ea
DSA Soldier <sudeepchatterje70@gmail.com>
2026-02-09 18:55:29 +0530

    day five reactive and template driven form complete

--- my-angular-16-project-global/src/app/services/testing.service.ts
+++ my-angular-16-project-global/src/app/services/testing.service.ts
@@ -1,14 +1,18 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestingService {
 
-  private data :string[] = ['parent service','child service']
-  constructor() { }
+  private data :string[] = ['parent service','child service'];
+
+  constructor(private http: HttpClient) { }
 
   getData(): string[]{
     return this.data;
   }
+
 }
